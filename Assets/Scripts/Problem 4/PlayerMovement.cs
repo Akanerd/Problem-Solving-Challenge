26139598f26591a4eb7637adfbee9fe6784114c6
Muhammad Hafizh Azzasafah <85096618+Akanerd@users.noreply.github.com>
2021-10-09 19:14:28 +0700

    Penambahan komentar

--- Assets/Scripts/Problem 4/PlayerMovement.cs
+++ Assets/Scripts/Problem 4/PlayerMovement.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Inheritance Mengambil kelas Movement
 public class PlayerMovement : Movement
 {
     // Update is called once per frame
     void Update()
     {
+        //Movement Player by Input Key - Movemet Horizontal
         movement.x = Input.GetAxisRaw("Horizontal");
+        //Movement Player by Input Key -Movement Vertical
         movement.y= Input.GetAxisRaw("Vertical");
+
         IsMovement = movement !=  Vector2.zero ? true : false;
     }
 }
