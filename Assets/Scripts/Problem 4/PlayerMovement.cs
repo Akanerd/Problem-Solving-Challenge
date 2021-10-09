626ddca180ff5c771c2e93a2d7071ea611216284
Muhammad Hafizh Azzasafah <85096618+Akanerd@users.noreply.github.com>
2021-10-09 19:09:14 +0700

    Problem 4 - Modifikasi dari nomor 2, buatlah lingkaran bergerak dengan input atas bawah kiri kanan.

---
+++ Assets/Scripts/Problem 4/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Movement
+{
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y= Input.GetAxisRaw("Vertical");
+        IsMovement = movement !=  Vector2.zero ? true : false;
+    }
+}
