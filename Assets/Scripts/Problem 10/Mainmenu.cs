550fdc11e03c95e3a08e6f119726e94cf6ebe795
Muhammad Hafizh Azzasafah <85096618+Akanerd@users.noreply.github.com>
2021-10-10 20:03:15 +0700

    Upload Final Problem 10

---
+++ Assets/Scripts/Problem 10/Mainmenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
